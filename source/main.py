95d2cbd3b589148d92e1016c89f0214af4208db1
Kael Paraguassu <kaelparaguassu@hotmail.com>
2024-05-08 11:51:07 -0300

    comentando e validando entradas

--- source/main.py
+++ source/main.py
@@ -2,6 +2,7 @@ from avl_tree import ArvoreAVL
 from animal import Animal, Historico, Registro
 import sys
 import json
+import datetime
 
 def consultar_animal(arvore, chave):
     node = arvore.consultar(chave)
@@ -12,41 +13,112 @@ def consultar_animal(arvore, chave):
     input("Pressione Enter para continuar...")  
 
 def adicionar_animal(arvore):
+    # Solicita o ID do animal
     id = int(input("Digite o ID do animal: "))
+    # Verifica se o ID já existe na árvore
     while arvore.consultar(id):
         print("")
         print("Animal com ID já existe. Por favor, escolha outro ID.")
         id = input("Digite o ID do animal: ")
 
+    # Solicita o apelido do animal
     apelido = input("Digite o apelido do animal: ")
-    inicio_monitoramento = input("Digite a data de início do monitoramento do animal: ")
+
+    # Solicita a data de início do monitoramento do animal e verifica validade do formato
+    while True:
+        inicio_monitoramento = input("Digite a data de início do monitoramento do animal (DD/MM/AAAA): ")
+        try:
+            datetime.datetime.strptime(inicio_monitoramento, "%d/%m/%Y")
+            break
+        except ValueError:
+            print("Formato de data inválido. Por favor, digite novamente.")
+
+    # Solicita a espécie do animal
     especie = input("Digite a espécie do animal: ")
-    sexo = input("Digite o sexo do animal: ")
-    data_nascimento = input("Digite a data de nascimento do animal: ")
+
+    # Solicita o sexo do animal
+    sexo = input("Digite o sexo do animal (M/F): ")
+    # Verifica se o sexo é válido
+    while sexo not in ["M", "F"]:
+        print("Valor inválido. Por favor, digite novamente.")
+        sexo = input("Digite o sexo do animal: ")
+
+    # Solicita a data de nascimento do animal e verifica validade do formato
+    while True:
+        data_nascimento = input("Digite a data de nascimento do animal: (DD/MM/AAAA): ")
+        try:
+            datetime.datetime.strptime(data_nascimento, "%d/%m/%Y")
+            break
+        except ValueError:
+            print("Formato de data inválido. Por favor, digite novamente.")
 
     print("")
+    # Pergunta se deseja adicionar um registro no histórico
     add_history = input("Deseja adicionar um registro no histórico? (S/N): ")
     if add_history.lower() == "s":
-        data_avaliacao = input("Digite a data da avaliação: ")
-        temperatura = input("Digite a temperatura: ")
-        peso = input("Digite o peso: ")
-        altura = input("Digite a altura: ")
-        amostra = input("Digite a amostra: ")
-        exame = input("Digite o exame: ")
-        problema = input("Digite o problema: ")
+        while True:
+            # Solicita a data da avaliação e verifica se está no formato correto
+            data_avaliacao = input("Digite a data da avaliação (DD/MM/AAAA): ")
+            try:
+                datetime.datetime.strptime(data_avaliacao, "%d/%m/%Y")
+                break
+            except ValueError:
+                print("Formato de data inválido. Por favor, digite novamente.")
+
+        while True:
+            # Solicita a temperatura e verifica se é um valor válido
+            temperatura = input("Digite a temperatura (C°): ")
+            if temperatura.isdigit():
+                break
+            else:
+                print("Valor inválido. Por favor, digite novamente.")
+
+        while True:
+            # Solicita o peso e verifica se é um valor válido
+            peso = input("Digite o peso (Kg): ")
+            if peso.isdigit():
+                break
+            else:
+                print("Valor inválido. Por favor, digite novamente.")
+
+        while True:
+            # Solicita a altura e verifica se é um valor válido
+            altura = input("Digite a altura (Cm): ")
+            if altura.isdigit():
+                break
+            else:
+                print("Valor inválido. Por favor, digite novamente.")
+
+        # Pergunta se foi coletada amostra de sangue e se o exame físico está ok
+        amostra = input("Foi coletada amostra de sangue (S/N): ")
+        exame = input("O exame físico está ok? (S/N): ")
+        amostra = amostra.lower() == "s"
+        exame = exame.lower() == "s"
+        problema = None
+
+        if not exame:
+            # Se o exame não estiver ok, solicita o problema de saúde
+            problema = input("Digite o problema de saúde: ")
+
+        # Cria um objeto Registro com as informações fornecidas
         registro = Registro(data_avaliacao, temperatura, peso, altura, amostra, exame, problema)
+        # Cria um objeto Historico com o registro
         historico = Historico([registro])
     else:
+        # Cria um objeto Historico vazio
         historico = Historico([])
 
+    # Cria um objeto Animal com as informações fornecidas
     animal = Animal(id, apelido, inicio_monitoramento, especie, sexo, data_nascimento, historico)
 
+    # Insere o objeto Animal na árvore AVL
     arvore.inserir(animal)
 
 def remover_animal(arvore, chave):
     arvore.remover(chave)
 
 def adicionar_registro(arvore, chave):
+    # Consulta o nó correspondente à chave na árvore
     node = arvore.consultar(chave)
     if node:
         print("")
@@ -54,26 +126,68 @@ def adicionar_registro(arvore, chave):
         node.animal.print_info()
 
         print("")
+        # Pergunta ao usuário se deseja atualizar o histórico
         historico = input("Deseja atualizar o histórico? (S/N): ")
         if historico.lower() == "s":
-            data_avaliacao = input("Digite a data da avaliação: ")
-            temperatura = input("Digite a temperatura: ")
-            peso = input("Digite o peso: ")
-            altura = input("Digite a altura: ")
-            amostra = input("Digite a amostra: ")
-            exame = input("Digite o exame: ")
-            problema = input("Digite o problema: ")
+            while True:
+                # Solicita a data da avaliação e verifica se está no formato correto
+                data_avaliacao = input("Digite a data da avaliação (DD/MM/AAAA): ")
+                try:
+                    datetime.datetime.strptime(data_avaliacao, "%d/%m/%Y")
+                    break
+                except ValueError:
+                    print("Formato de data inválido. Por favor, digite novamente.")
 
+            while True:
+                # Solicita a temperatura e verifica se é um valor válido
+                temperatura = input("Digite a temperatura (C°): ")
+                if temperatura.isdigit():
+                    break
+                else:
+                    print("Valor inválido. Por favor, digite novamente.")
+
+            while True:
+                # Solicita o peso e verifica se é um valor válido
+                peso = input("Digite o peso (Kg): ")
+                if peso.isdigit():
+                    break
+                else:
+                    print("Valor inválido. Por favor, digite novamente.")
+
+            while True:
+                # Solicita a altura e verifica se é um valor válido
+                altura = input("Digite a altura (Cm): ")
+                if altura.isdigit():
+                    break
+                else:
+                    print("Valor inválido. Por favor, digite novamente.")
+
+            # Pergunta se foi coletada amostra de sangue e se o exame físico está ok
+            amostra = input("Foi coletada amostra de sangue (S/N): ")
+            exame = input("O exame físico está ok? (S/N): ")
+            amostra = amostra.lower() == "s"
+            exame = exame.lower() == "s"
+            problema = None
+
+            if not exame:
+                # Se o exame não estiver ok, solicita o problema de saúde
+                problema = input("Digite o problema de saúde: ")
+
+            # Cria um objeto Registro com as informações fornecidas
             registro = Registro(data_avaliacao, temperatura, peso, altura, amostra, exame, problema)
+            # Adiciona o registro ao histórico do animal
             node.animal.addLog(registro)
 
-def salvar_alteracoes(arvore, file_path):
-    with open("./fauna2.json", 'w') as file:
+# Função para salvar as alterações no arquivo
+def salvar_alteracoes(arvore, save_path):
+    with open(save_path, 'w') as file:
         animals = []
 
+        # Percorre todos os nós da árvore e adiciona os animais na lista
         for node in arvore:
             animals.append(node.animal)
 
+        # Cria um dicionário com a lista de animais e escreve no arquivo JSON
         json.dump({"animals": [a.to_dict() for a in animals]}, file, indent=4)
 
 def sair():
@@ -111,7 +225,7 @@ def menu(file_path):
 
             # Cria um objeto Animal com as informações
             animal = Animal(id, apelido, inicio_monitoramento, especie, sexo, data_nascimento, historico)
-            animal.print_info()
+            # animal.print_info()
 
             # # Insere o objeto Animal na árvore AVL
             arvore.inserir(animal)
@@ -139,7 +253,8 @@ def menu(file_path):
         elif choice == "4":
             adicionar_registro(arvore, int(input("Digite o ID do animal: "))) # OK?
         elif choice == "5":
-            salvar_alteracoes(arvore, file_path)
+            save_path = input("Digite o caminho do arquivo para salvar as alterações: ")
+            salvar_alteracoes(arvore, save_path)
         elif choice == "6":
             sair()
         else:
