415b3e0424fc8fc30c7be18cf803ae8f3a9fefcc
Kael Paraguassu <kaelparaguassu@hotmail.com>
2024-05-08 11:49:56 -0300

    comentando classes e ajustando formatacao

--- source/animal.py
+++ source/animal.py
@@ -1,5 +1,4 @@
-import json
-
+# Classe que representa um registro no histórico
 class Registro:
     def __init__(self, data_avaliacao, temperatura, peso, altura, amostra, exame, problema_saude):
         self.data_avaliacao = data_avaliacao
@@ -10,6 +9,7 @@ class Registro:
         self.exame = exame
         self.problema_saude = problema_saude
 
+    # Converte o registro em um dicionário
     def to_dict(self):
         return {
             "data_avaliacao": self.data_avaliacao,
@@ -22,22 +22,27 @@ class Registro:
         }
 
 
+# Classe que representa o histórico de registros
 class Historico:
     def __init__(self, log_list=None):
         if log_list is None:
             log_list = []
         self.log_list = log_list
 
+    # Adiciona um registro ao histórico
     def addLog(self, log):
         self.log_list.append(log)
 
+    # Retorna as informações do histórico
     def get_info(self):
         return self.log_list
 
+    # Converte o histórico em uma lista de dicionários
     def to_dict(self):
         return [log.to_dict() for log in self.log_list]
 
 
+# Classe que representa um animal
 class Animal:
     def __init__(self, id, apelido, inicio_monitoramento, especie, sexo, data_nascimento, historico=Historico([])):
         self.id = id
@@ -48,9 +53,11 @@ class Animal:
         self.data_nascimento = data_nascimento
         self.historico = historico
 
+    # Adiciona um registro ao histórico do animal
     def addLog(self, log):
         self.historico.addLog(log)
 
+    # Imprime as informações do animal
     def print_info(self):
         print(f"ID: {self.id}")
         print(f"Apelido: {self.apelido}")
@@ -61,12 +68,13 @@ class Animal:
 
         if self.historico:
             print("Histórico de Registros:")
-            print("Data\t\tTemperatura\t\tPeso\t\tAltura\t\tAmostra\t\tExame\t\tProblema de Saúde")
+            print("Data\t\tTemperatura\t\tPeso\t\tAltura\t\tAmostra?\t\tExame OK?\t\tProblema de Saúde")
             for log in self.historico.log_list:
-                print(f"{log.data_avaliacao}\t{log.temperatura}\t\t\t{log.peso}\t\t{log.altura}\t\t{log.amostra}\t\t{log.exame}\t{log.problema_saude}")
+                print(f"{log.data_avaliacao}\t{log.temperatura}\t\t\t{log.peso}\t\t{log.altura}\t\t{log.amostra}\t\t\t{log.exame}\t\t\t{log.problema_saude}")
         else:
             print("Não há histórico disponível.")
 
+    # Converte o animal em um dicionário
     def to_dict(self):
         return {
             "id": self.id,
