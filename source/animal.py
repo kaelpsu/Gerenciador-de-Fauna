efd0a82d1c18664d7a5a088d6e888ade6a0c583a
Kael Paraguassu <kaelparaguassu@hotmail.com>
2024-10-08 22:27:58 -0300

    removendo comentários desnecessários e renomeando funções

--- source/animal.py
+++ source/animal.py
@@ -1,16 +1,14 @@
-# Classe que representa um registro no histórico
 class Registro:
-    def __init__(self, data_avaliacao, temperatura, peso, altura, amostra, exame, problema_saude):
+    def __init__(self, data_avaliacao, temperatura, peso, altura, amostra, exame, problema_de_saude):
         self.data_avaliacao = data_avaliacao
         self.temperatura = temperatura
         self.peso = peso
         self.altura = altura
         self.amostra = amostra
         self.exame = exame
-        self.problema_saude = problema_saude
+        self.problema_de_saude = problema_de_saude
 
-    # Converte o registro em um dicionário
-    def to_dict(self):
+    def convert_to_dictionary(self):
         return {
             "data_avaliacao": self.data_avaliacao,
             "temperatura": self.temperatura,
@@ -18,31 +16,26 @@ class Registro:
             "altura": self.altura,
             "amostra": self.amostra,
             "exame": self.exame,
-            "problema_saude": self.problema_saude
+            "problema_de_saude": self.problema_de_saude
         }
 
 
-# Classe que representa o histórico de registros
 class Historico:
-    def __init__(self, log_list=None):
-        if log_list is None:
-            log_list = []
-        self.log_list = log_list
+    def __init__(self, logs=None):
+        if logs is None:
+            logs = []
+        self.logs = logs
 
-    # Adiciona um registro ao histórico
-    def addLog(self, log):
-        self.log_list.append(log)
+    def add_log(self, log):
+        self.logs.append(log)
 
-    # Retorna as informações do histórico
     def get_info(self):
-        return self.log_list
+        return self.logs
 
-    # Converte o histórico em uma lista de dicionários
-    def to_dict(self):
-        return [log.to_dict() for log in self.log_list]
+    def convert_to_dictionary(self):
+        return [log.convert_to_dictionary() for log in self.logs]
 
 
-# Classe que representa um animal
 class Animal:
     def __init__(self, id, apelido, inicio_monitoramento, especie, sexo, data_nascimento, historico=Historico([])):
         self.id = id
@@ -53,11 +46,16 @@ class Animal:
         self.data_nascimento = data_nascimento
         self.historico = historico
 
-    # Adiciona um registro ao histórico do animal
-    def addLog(self, log):
-        self.historico.addLog(log)
+    def add_log(self, log):
+        self.historico.add_log(log)
+        
+    def print_historico(self):
+        print("Histórico de Registros:")
+        print("Data\t\tTemperatura\t\tPeso\t\tAltura\t\tAmostra?\t\tExame OK?\t\tProblema de Saúde")
 
-    # Imprime as informações do animal
+        for log in self.historico.logs:
+            print(f"{log.data_avaliacao}\t{log.temperatura}\t\t\t{log.peso}\t\t{log.altura}\t\t{log.amostra}\t\t\t{log.exame}\t\t\t{log.problema_de_saude}")
+
     def print_info(self):
         print(f"ID: {self.id}")
         print(f"Apelido: {self.apelido}")
@@ -67,15 +65,11 @@ class Animal:
         print(f"Data de Nascimento: {self.data_nascimento}")
 
         if self.historico:
-            print("Histórico de Registros:")
-            print("Data\t\tTemperatura\t\tPeso\t\tAltura\t\tAmostra?\t\tExame OK?\t\tProblema de Saúde")
-            for log in self.historico.log_list:
-                print(f"{log.data_avaliacao}\t{log.temperatura}\t\t\t{log.peso}\t\t{log.altura}\t\t{log.amostra}\t\t\t{log.exame}\t\t\t{log.problema_saude}")
+            self.print_historico()
         else:
             print("Não há histórico disponível.")
 
-    # Converte o animal em um dicionário
-    def to_dict(self):
+    def convert_to_dictionary(self):
         return {
             "id": self.id,
             "apelido": self.apelido,
@@ -83,5 +77,5 @@ class Animal:
             "especie": self.especie,
             "sexo": self.sexo,
             "data_nascimento": self.data_nascimento,
-            "historico": self.historico.to_dict()
+            "historico": self.historico.convert_to_dictionary(),
         }
